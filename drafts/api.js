f4ddce1f2a50b2089c389603855ca8c45aa9ba7d
Thomas Di Grégorio <thomas@devingfx.com>
2018-07-30 18:05:15 +0200

    Update api.js

--- drafts/api.js
+++ drafts/api.js
@@ -1,5 +1,28 @@
 /* draft */
 
+const API = (ss,...pp)=> new Proxy(
+	Object.assign( ()=>0, {
+		url: stick(ss,...pp)
+	,	get GET(){ console.log('GET::'+this.url) }
+	}), {
+		get:(o,p)=> typeof p == 'symbol' || Reflect.has(o,p) ? Reflect.get(o,p) : API`${o.url}/${p}`
+	,	apply:(o,t,a)=> console.log('CALL::',o,t,a)
+	})
+
+
+// exemples
+
+let randomuser = API`https://randomuser.me`
+randomuser.api['?results=5&nat=FR'].GET
+
+let g1 = API`https://g1.duniter.org`
+( await g1.wot.members ).results.map( ({pubkey})=> g1.wot.lookup[pubkey] )
+
+let kumuniter = API`https://kumuniter.devingfx.com`
+kumuniter.assets.data`wot.kumu.json`
+
+// other exemples
+
 api.wot.members.then( o=> o.map( m=> scene.add(new THREEx.Planet(m)) ) )
 api.wot.members`o=> o.map( m=> scene.add(new THREEx.Planet(m)) )`
 
